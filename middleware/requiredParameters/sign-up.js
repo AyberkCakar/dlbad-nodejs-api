7b1c645d4f992577693997222aa1e90bdaa09c16
AyberkCakar <ayberkcakar35@gmail.com>
2023-10-17 02:18:12 +0300

    fix: fixed signUp validation

--- middleware/requiredParameters/sign-up.js
+++ middleware/requiredParameters/sign-up.js
@@ -6,8 +6,8 @@ module.exports = {
       input: joi.object().keys({
         emailAddress: joi.string().email().max(200).required(),
         password: joi.string().min(6).max(99).required(),
-        name: joi.string().required(),
-        role: joi.string(),
+        firstName: joi.string().required(),
+        lastName: joi.string().required(),
       }),
     }),
   }),
